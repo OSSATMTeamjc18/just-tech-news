f5809a31b163070e47b1982662e85995e1ef58ec
Brady Taylor <baetay03@gmail.com>
2021-07-14 21:49:40 -0600

    password for user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // get all users
-router.get('/api/users', (req, res) => {
+router.get('/', (req, res) => {
   User.findAll({
     attributes: { exclude: ['password'] }
   })
@@ -13,7 +13,7 @@ router.get('/api/users', (req, res) => {
     });
 });
 
-router.get('/api/users/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   User.findOne({
     attributes: { exclude: ['password'] },
     where: {
@@ -33,7 +33,7 @@ router.get('/api/users/:id', (req, res) => {
     });
 });
 
-router.post('/api/users', (req, res) => {
+router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -47,11 +47,35 @@ router.post('/api/users', (req, res) => {
     });
 });
 
-router.put('/api/users/:id', (req, res) => {
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
+router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
@@ -69,7 +93,7 @@ router.put('/api/users/:id', (req, res) => {
     });
 });
 
-router.delete('/api/users/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
       id: req.params.id
@@ -89,3 +113,4 @@ router.delete('/api/users/:id', (req, res) => {
 });
 
 module.exports = router;
+
